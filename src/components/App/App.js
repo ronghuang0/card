8a00da1ad6dcc7e686c3ae8005254b65e4783e92
Rong <rongster3544@gmail.com>
2018-02-01 03:36:54 -0800

    cycle computer choice

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-did-update-set-state */
 import React, { Component } from 'react';
 import Counter from '../Counter/Counter';
 import Battlefield from '../Battlefield/Battlefield';
@@ -11,22 +12,37 @@ export default class App extends Component {
     this.state = {
       playerWins: 0,
       computerWins: 0,
+      computerSelection: 0,
     };
     this.weaponClick = this.weaponClick.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.stage === 'cycle') {
+      setTimeout(() => {
+        console.log('woley', prevState.computerSelection);
+        this.setState({ computerSelection: (prevState.computerSelection + 1) % 3 });
+      }, 100);
+    }
   }
 
   weaponClick(weapon) {
     const computerSelection = simulateComputer();
     // 1 means first argument wins, -1 means second argument wins, 0 is tie
     const winner = compare(weapon, computerSelection);
-    this.setState(({ playerWins, computerWins }) => (
-      {
-        playerSelection: weapon,
+    setTimeout(() => {
+      this.setState(({ playerWins, computerWins }) => ({
+        stage: 'result',
         computerSelection,
         playerWins: winner === 1 ? playerWins + 1 : playerWins,
         computerWins: winner === -1 ? computerWins + 1 : computerWins,
-      }
-    ));
+      }));
+    }, 3000);
+    this.setState({
+      playerSelection: weapon,
+      computerSelection: 0,
+      stage: 'cycle',
+    });
   }
   render() {
     const {
