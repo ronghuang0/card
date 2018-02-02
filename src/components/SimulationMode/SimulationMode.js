fd8514d4abbc9d9271e9feb72fcfa371602c61df
Rong <rongster3544@gmail.com>
2018-02-02 01:15:14 -0800

    fixing up simulationmode

--- src/components/SimulationMode/SimulationMode.js
+++ src/components/SimulationMode/SimulationMode.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Counter from '../Counter/Counter';
 import Battlefield from '../Battlefield/Battlefield';
-import WeaponSelect from '../WeaponSelect/WeaponSelect';
 import { simulateComputer, computeWinner } from '../../utils';
 import style from './SimulationMode.css';
 
@@ -9,36 +8,47 @@ export default class SimulationMode extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerWins: 0,
-      computerWins: 0,
-      playerSelection: undefined,
-      computerSelection: undefined,
+      firstWins: 0,
+      secondWins: 0,
+      firstSelection: undefined,
+      secondSelection: undefined,
     };
     this.reset = this.reset.bind(this);
   }
 
   reset() {
     this.setState({
-      playerWins: 0,
-      computerWins: 0,
-      playerSelection: undefined,
-      computerSelection: undefined,
+      firstWins: 0,
+      secondWins: 0,
+      firstSelection: undefined,
+      secondSelection: undefined,
     });
+  }
+  simulateGame() {
+    const firstSelection = simulateComputer();
+    const secondSelection = simulateComputer();
+    const result = computeWinner(firstSelection, secondSelection);
+    this.setState(({ firstWins, secondWins }) => ({
+      firstSelection,
+      secondSelection,
+      firstWins: result === 1 ? firstWins + 1 : firstWins,
+      secondWins: result === -1 ? secondWins + 1 : secondWins,
+    }));
   }
 
   render() {
     const {
-      playerWins,
-      computerWins,
-      playerSelection,
-      computerSelection,
+      firstWins,
+      secondWins,
+      firstSelection,
+      secondSelection,
     } = this.state;
     const { toggleMode } = this.props;
 
     return (
       <div>
         <div className={style.counterContainer}>
-          <Counter name='Cpu 1' wins={playerWins} />
+          <Counter name='Cpu 1' wins={firstWins} />
           <div
             className={style.reset}
             onClick={this.reset}
@@ -51,13 +61,13 @@ export default class SimulationMode extends Component {
           >
             Player Mode
           </div>
-          <Counter name='Cpu 2' wins={computerWins} />
+          <Counter name='Cpu 2' wins={secondWins} />
         </div>
         <Battlefield
           firstName='Cpu 1'
           secondName='Cpu 2'
-          firstSelection={playerSelection}
-          secondSelection={computerSelection}
+          firstSelection={firstSelection}
+          secondSelection={secondSelection}
         />
       </div>
     );
