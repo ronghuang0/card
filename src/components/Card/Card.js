92aec0c4c5c95c709a9823544d5113be8d8f7664
Rong <rongster3544@gmail.com>
2018-06-27 02:20:11 -0700

    drag

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,15 @@
 /* eslint-disable */
 import React from 'react';
 import { Motion, spring } from 'react-motion';
-import range from 'lodash.range';
 import cardStyle from './Card.css';
-
-function reinsert(arr, from, to) {
-  const _arr = arr.slice(0);
-  const val = _arr[from];
-  _arr.splice(from, 1);
-  _arr.splice(to, 0, val);
-  return _arr;
-}
+import range from 'lodash.range';
 
 function clamp(n, min, max) {
   return Math.max(Math.min(n, max), min);
 }
 
 const springConfig = {stiffness: 300, damping: 50};
-const itemsCount = 4;
+const itemsCount = 2;
 
 export default class Demo extends React.Component {
   constructor(props) {
@@ -27,7 +19,6 @@ export default class Demo extends React.Component {
       mouseY: 0,
       isPressed: false,
       originalPosOfLastPressed: 0,
-      order: range(itemsCount),
     };
   };
 
@@ -53,47 +44,68 @@ export default class Demo extends React.Component {
       mouseY: pressY,
       isPressed: true,
       originalPosOfLastPressed: pos,
+      originalPageY: pageY,
     });
   };
 
   handleMouseMove = ({pageY}) => {
-    const {isPressed, topDeltaY, order, originalPosOfLastPressed} = this.state;
+    const {isPressed, topDeltaY, originalPosOfLastPressed} = this.state;
 
     if (isPressed) {
       const mouseY = pageY - topDeltaY;
       const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1);
-      let newOrder = order;
 
-      if (currentRow !== order.indexOf(originalPosOfLastPressed)){
-        newOrder = reinsert(order, order.indexOf(originalPosOfLastPressed), currentRow);
-      }
 
-      this.setState({mouseY: mouseY, order: newOrder});
+      this.setState({mouseY: mouseY});
     }
   };
 
-  handleMouseUp = () => {
-    this.setState({isPressed: false, topDeltaY: 0});
+  handleMouseUp = ({pageY}) => {
+    const { mouseY, topDeltaY, originalPageY} = this.state;
+    console.log('page', pageY);
+    console.log('orig', originalPageY);
+
+    this.setState({isPressed: false, topDeltaY: 0, pageY: pageY});
   };
 
   render() {
-    const {mouseY, isPressed, originalPosOfLastPressed, order} = this.state;
+    const {mouseY, isPressed, originalPosOfLastPressed, originalPageY, pageY} = this.state;
 
     return (
       <div className={cardStyle.demoOuter}>
         <div className={cardStyle.demo}>
           {range(itemsCount).map(i => {
-            const style = originalPosOfLastPressed === i && isPressed
-              ? {
+            let style;
+            if (i === 0) {
+              style = {
+                  scale: spring(1, springConfig),
+                  shadow: spring(1, springConfig),
+                  y: spring(i * 100, springConfig),
+                };
+            }
+            if (i === 1) {
+              if (originalPosOfLastPressed === 1 && isPressed) {
+                style = {
                   scale: spring(1.1, springConfig),
                   shadow: spring(16, springConfig),
                   y: mouseY,
-                }
-              : {
-                  scale: spring(1, springConfig),
-                  shadow: spring(1, springConfig),
-                  y: spring(order.indexOf(i) * 100, springConfig),
                 };
+              } else {
+                if(originalPageY - pageY > 50) {
+                  style = {
+                      scale: spring(1, springConfig),
+                      shadow: spring(1, springConfig),
+                      y: spring(0, springConfig),
+                    };
+                } else {
+                  style = {
+                    scale: spring(1, springConfig),
+                    shadow: spring(1, springConfig),
+                    y: spring(i * 100, springConfig),
+                  };
+                }
+              }
+            }
             return (
               <Motion style={style} key={i}>
                 {({scale, shadow, y}) =>
@@ -107,7 +119,7 @@ export default class Demo extends React.Component {
                       WebkitTransform: `translate3d(0, ${y}px, 0) scale(${scale})`,
                       zIndex: i === originalPosOfLastPressed ? 99 : i,
                     }}>
-                    {order.indexOf(i) + 1}
+                    {i + 1}
                   </div>
                 }
               </Motion>
