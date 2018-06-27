aba650cbc874aab331729c137fc4da9c5148eece
Rong <rong.huang2@nike.com>
2018-06-27 16:50:50 -0700

    Remove touch actions

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,6 @@ import { Motion, spring } from 'react-motion';
 import cardStyle from './Card.css';
 import range from 'lodash.range';
 
-function clamp(n, min, max) {
-  return Math.max(Math.min(n, max), min);
-}
-
 const springConfig = {stiffness: 300, damping: 50};
 const itemsCount = 2;
 
@@ -22,9 +18,14 @@ export default class Demo extends React.Component {
   };
 
   componentDidMount() {
-    window.addEventListener('touchmove', this.handleTouchMove);
+    window.addEventListener('touchmove', this.handleTouchMove, { passive: false });
     window.addEventListener('touchend', this.handleTouchEnd);
+    window.addEventListener('touchstart', this.touchStartPreventDefault, { passive: false});
   };
+
+  touchStartPreventDefault = (e) => {
+    e.preventDefault();
+  }
 
   handleTouchStart = (pos, pressY, e) => {
     const pageY = e.touches[0].pageY;
@@ -38,27 +39,22 @@ export default class Demo extends React.Component {
   };
 
   handleTouchMove = (e) => {
-    // e.preventDefault();
     const pageY = e.touches[0].pageY;
     const {isPressed, topDeltaY, originalPosOfLastPressed} = this.state;
     if (isPressed) {
       const mouseY = pageY - topDeltaY;
-      const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1);
       this.setState({mouseY: mouseY, pageY: pageY});
     }
   };
 
   handleTouchEnd = (e) => {
-    console.log('woley', e);
-    const { mouseY, topDeltaY} = this.state;
-    // console.log('page', pageY);
-    // console.log('orig', originalPageY);
-
-    this.setState({isPressed: false, topDeltaY: 0 });
+    this.setState({isPressed: false});
   };
 
   render() {
     const {mouseY, isPressed, originalPosOfLastPressed, originalPageY, pageY} = this.state;
+    console.log('mouseY', mouseY);
+    console.log('pageY', pageY);
 
     return (
       <div className={cardStyle.demoOuter}>
